3c65c64e2f971aaa378282c0eb429127a9277bfe
Julie Koubová <juliekoubova@icloud.com>
2015-09-22 12:05:40 +0200

    ImportSingleContact

---
+++ src/exchange2crm.Grains/ImportSingleContact.fs
@@ -0,0 +1,16 @@
+﻿namespace exchange2crm.Grains
+
+open System
+open System.Threading.Tasks
+open Orleans
+open exchange2crm.Interfaces
+open exchange2crm
+
+type ImportSingleContact() = 
+    inherit Orleans.Grain()
+
+    interface IImportSingleContact with
+        member this.Import (contact : IContact) =
+            Task.Factory.StartNew(
+                fun () -> Xrm.createContact(contact)
+            )
