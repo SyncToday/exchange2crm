c6dd8db8f7373226562e5eb264e89ad03674a35e
davidpodhola <david.podhola@naseukoly.cz>
2015-09-23 10:01:29 +0200

    ConnectionStrings.config fix

--- buildAppVeyor.cmd
+++ buildAppVeyor.cmd
@@ -3,5 +3,6 @@ cls
 
 ECHO F| xcopy "tests\exchange2crm.Tests\appSettings.sample.config" "tests\exchange2crm.Tests\appSettings.config" /i /d /y
 ECHO F|xcopy "src\exchange2crm\Secret.Sample.fs" "src\exchange2crm\Secret.fs" /i /d /y
+ECHO F|xcopy "src\exchange2crm.WebJob\ConnectionStrings.Sample.config" "src\exchange2crm.WebJob\ConnectionStrings.config" /i /d /y
 
 build.cmd
